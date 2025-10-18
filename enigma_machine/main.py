615ec0b0b168460ad0f9de9891612d820d188dc8
DevP3try <devp3try@gmail.com>
2025-10-18 20:38:24 -0300

    Escrevendo o codigo do rotor com class

---
+++ enigma_machine/main.py
@@ -0,0 +1,34 @@
+from rotor import Rotor
+
+# Configuração da Máquina:
+# Rotores: I, II, III (Lento, Meio, Rápido)
+# Posições Iniciais: A, A, A
+# Configuração dos Anéis: A, A, A (ou 01, 01, 01)
+# Refletor: B
+
+# Nota: O texto 'OLAMUNDO' tem 'M'. Vamos testar com 'AAAAA' para ver o giro
+texto = "AAAAA"
+
+# 1. Cifrando
+maquina_cifra = Rotor(
+    rotores_escolhidos = ['I', 'II', 'III'],
+    posicoes_iniciais = ['A', 'A', 'Z'], # Perto de um entalhe!
+    config_aneis       = ['A', 'A', 'A'],
+    refletor_escolhido = 'B'
+)
+print(f"Configuração: I-II-III | Posições: AAZ | Anéis: AAA | Refletor: B")
+cifrado = maquina_cifra.criptografar_texto(texto)
+print(f"Texto original: {texto}")
+print(f"Texto cifrado:  {cifrado}") # Deve ser 'BDZGO'
+
+
+# 2. Decifrando (a Enigma é recíproca)
+# Basta criar uma máquina EXATAMENTE IGUAL
+maquina_decifra = Rotor(
+    rotores_escolhidos = ['I', 'II', 'III'],
+    posicoes_iniciais = ['A', 'A', 'Z'],
+    config_aneis       = ['A', 'A', 'A'],
+    refletor_escolhido = 'B'
+)
+decifrado = maquina_decifra.criptografar_texto(cifrado)
+print(f"Texto decifrado:{decifrado}") # Deve ser 'AAAAA'
