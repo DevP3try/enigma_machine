22989a492c8c28382baa33479a148fe0c4b20216
Willian <willianmartinsgomes@gmail.com>
2025-10-18 22:18:47 -0300

    adicionando plugboard funcionando

--- enigma_machine/plugboard.py
+++ enigma_machine/plugboard.py
@@ -1,6 +1,22 @@
-def plugboard(chave):
-    print(chave)
-    return
+def plugboard(chave_plug, txt_to_crypt):
 
-config_plug = {"h": "k", "l": "p", "j": "b", "a": "z", "f": "i"}
-plugboard({config_plug})
+    chave = {}
+    config_plug = chave_plug.replace(" ","")
+    chave_inv = {}
+    crypted_txt = ""
+
+    for i in range(0,len(config_plug),2):
+        chave.update({config_plug[i]: config_plug[i+1]})
+    for i in range(0,len(config_plug),2):
+        chave_inv.update({config_plug[i+1]: config_plug[i]})
+
+    for letra in txt_to_crypt:
+        if letra in chave:
+            crypted_txt += chave[letra]
+        elif letra in chave_inv:
+            crypted_txt += chave_inv[letra]
+        else:
+            crypted_txt += letra
+            
+    return (crypted_txt)
+            
